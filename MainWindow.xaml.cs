02e140e100f92212fdbfd68df30e40d7fdfb52ad
kormonditamas2222 <t.kormond2006@gmail.com>
2025-11-19 10:41:39 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kviz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string nev;
+        
+        private void btn_nev_Click(object sender, RoutedEventArgs e)
+        {
+            nev = tbox_nev.Text;
+            stack_start.Visibility = Visibility.Hidden;
+            dock_quiz.Visibility = Visibility.Visible;
+        }
+    }
+}
