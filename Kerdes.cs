2ba45f66a4088c15448059e53403a8054e94e133
xhBIROhx <birorihard@gmail.com>
2025-11-22 17:28:58 +0100

    működik

---
+++ Kerdes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kviz
+{
+    class Kerdes
+    {
+        private Random rand = new Random();
+
+
+        private string question;
+        private string[] answers;
+        private int correct;
+
+        public Kerdes(string question, string[] answers)
+        {
+            this.question = question;
+            this.answers = answers;
+            this.correct = 0;
+        }
+
+        public string Question { get => question; }
+        public string[] Answers { get => answers; }
+        public int Correct { get => correct; }
+
+        public void Shuffle() {
+            int[] numbers = Enumerable.Range(0, 4).ToArray();
+            numbers = numbers.OrderBy(x => rand.Next()).ToArray();
+
+            List<string> newlist = [];
+            foreach (int i in numbers)
+            {
+                newlist.Add(answers[i]);
+                if (i == 0)
+                {
+                    correct = newlist.Count()-1;
+                }
+            }
+            answers = newlist.ToArray();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.IO;
+using System.Printing;
 
 namespace kviz
 {
@@ -16,17 +19,95 @@ namespace kviz
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random rand = new Random();
+
+
+        private string nev;
+        private Kerdes[] Kerdesek = new Kerdes[10];
+        private int score = 0;
+        private int progress = 0;
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        string nev;
         
         private void btn_nev_Click(object sender, RoutedEventArgs e)
         {
-            nev = tbox_nev.Text;
+            nev = tbox_nev.Text.Trim();
             stack_start.Visibility = Visibility.Hidden;
             dock_quiz.Visibility = Visibility.Visible;
+
+            get_questions();
+            load_next_question();
+        }
+
+        private void get_questions()
+        {
+            List<Kerdes> all_questions = [];
+            using (StreamReader reader = new StreamReader("../../../kerdesek.txt"))
+            {
+                reader.ReadLine();
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(';');
+                    all_questions.Add(new Kerdes(
+                        parts[0],
+                        [
+                            parts[1], parts[2], parts[3], parts[4]
+                        ]
+                    ));
+                }
+            }
+
+            int[] numbers = Enumerable.Range(0, 10).ToArray();
+            numbers = numbers.OrderBy(x => rand.Next()).ToArray();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Kerdesek[i] = all_questions[numbers[i]];
+            }
+        }
+
+        private void load_next_question()
+        {
+            if (progress == 10) {
+                print_final();
+                return;
+            }
+            Kerdes kerdes = Kerdesek[progress];
+            kerdes.Shuffle();
+
+            lb_szamlalo.Content = (progress + 1).ToString();
+
+            tbox_kerdes.Text = kerdes.Question;
+
+            btn_a.Content = kerdes.Answers[0];
+            btn_b.Content = kerdes.Answers[1];
+            btn_c.Content = kerdes.Answers[2];
+            btn_d.Content = kerdes.Answers[3];
+        }
+
+        private void answer_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button pressed = (Button)sender;
+            int selected = int.Parse(pressed.Uid);
+            if (selected == Kerdesek[progress].Correct) {
+                score++;
+            }
+
+            progress++;
+            load_next_question();
+        }
+
+        private void print_final()
+        {
+            tbox_eredmeny.Text = score.ToString();
+            dock_quiz.Visibility = Visibility.Hidden;
+            stack_end.Visibility = Visibility.Visible;
         }
     }
 }
